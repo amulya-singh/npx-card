242d29619ffb055a7efc494047cf0d789559a0c0
Amulya Singh <amulya.singh125@gmail.com>
2022-10-09 02:13:00 +0530

    version2.1.1

---
+++ build.js
@@ -0,0 +1,113 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const boxen = require("boxen");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const clear = require("clear");
+const open = require("open");
+const fs = require("fs");
+const request = require("request");
+const path = require("path");
+const ora = require("ora");
+const cliSpinners = require("cli-spinners");
+clear();
+
+const prompt = inquirer.createPromptModule();
+
+const questions = [
+  {
+    type: "list",
+    name: "action",
+    message: "What you want to do?",
+    choices: [
+      {
+        name: `Send me an ${chalk.green.bold("email")}?`,
+        value: () => {
+          open(
+            "https://mail.google.com/mail/u/?authuser=amulya.singh125@gmail.com"
+          );
+          console.log("\nDone, see you soon at inbox.\n");
+        },
+      },
+      {
+        name: `Download my ${chalk.magentaBright.bold("Resume")}?`,
+        value: () => {
+          // cliSpinners.dots;
+          const loader = ora({
+            text: " Downloading Resume",
+            spinner: cliSpinners.material,
+          }).start();
+          let pipe = request(
+            "https://drive.google.com/file/d/1zdg_JObZqxeWxBPR8TYYbQ9-jmyszLsi/view"
+          ).pipe(fs.createWriteStream("./amy-resume.html"));
+          pipe.on("finish", function () {
+            let downloadPath = path.join(process.cwd(), "amy-resume.html");
+            console.log(`\nResume Downloaded at ${downloadPath} \n`);
+            open(downloadPath);
+            loader.stop();
+          });
+        },
+      },
+      {
+        name: "Just quit.",
+        value: () => {
+          console.log("Hasta la vista.\n");
+        },
+      },
+    ],
+  },
+];
+
+const data = {
+  name: chalk.bold.green("             Amulya Singh"),
+  handle: chalk.white("@amulya-singh"),
+  work: `${chalk.white("Student at ")} ${chalk
+    .hex("#2b82b2")
+    .bold(" Dr. Abdul kalam Technical University")}`,
+  github: chalk.gray("https://github.com/") + chalk.green("amulya-singh"),
+  linkedin:
+    chalk.gray("https://linkedin.com/in/") +
+    chalk.blue("amulya-singh12"),
+  npx: chalk.red("npx") + " " + chalk.white("amulyasingh"),
+
+  labelWork: chalk.white.bold("       Work:"),
+  labelGitHub: chalk.white.bold("     GitHub:"),
+  labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+  labelCard: chalk.white.bold("       Card:"),
+};
+
+const me = boxen(
+  [
+    `${data.name}`,
+    ``,
+    `${data.labelWork}  ${data.work}`,
+    ``,
+    `${data.labelGitHub}  ${data.github}`,
+    `${data.labelLinkedIn}  ${data.linkedin}`,
+    ``,
+    `${data.labelCard}  ${data.npx}`,
+    ``,
+    `${chalk.italic("I am currently looking for new opportunities,")}`,
+    `${chalk.italic("my inbox is always open. Whether you have a")}`,
+    `${chalk.italic("question or just want to say hi, I will try ")}`,
+    `${chalk.italic("my best to get back to you!")}`,
+  ].join("\n"),
+  {
+    margin: 1,
+    float: "center",
+    padding: 1,
+    borderStyle: "single",
+    borderColor: "green",
+  }
+);
+
+console.log(me);
+const tip = [
+  `Tip: Try ${chalk.cyanBright.bold("cmd/ctrl + click")} on the links above`,
+  "",
+].join("\n");
+console.log(tip);
+
+prompt(questions).then((answer) => answer.action());
